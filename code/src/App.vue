9fb76de9b5b93e9b42d0bd5a14d1dfcba98493eb
Carlos Eduardo <cadu.ce3@gmail.com>
2023-06-09 19:08:05 -0300

    colocando form dentro do BestTransport

--- code/src/App.vue
+++ code/src/App.vue
@@ -1,19 +1,16 @@
 <template>
   <div id="app">
     <BestTransport/>
-    <FormularioOne/>
   </div>
 </template>
 
 <script>
 import BestTransport from './components/BestTransport.vue'
-import FormularioOne from './components/FormularioOne.vue'
 
 export default {
   name: 'App',
   components: {
     BestTransport,
-    FormularioOne
   }
 }
 </script>
